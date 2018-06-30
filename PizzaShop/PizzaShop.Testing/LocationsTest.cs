bd3168ca8521210ea8127731189e29ab48e47be2
Brandon Kirk <bjkirk94@gmail.com>
2018-06-29 23:20:44 -0400

    Added LocationTesting.cs class along with first test

---
+++ PizzaShop/PizzaShop.Testing/LocationsTest.cs
@@ -0,0 +1,25 @@
+﻿using PizzaShop.Library;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace PizzaShop.Testing
+{
+    public class LocationsTest
+    {
+        [Fact]
+        public void AddStockShouldAddItemToDictionary()
+        {
+            // Arrange
+            var location = new Location();
+            var peppers = new Topping("peppers", 1);
+
+            //Act
+            location.AddStock(peppers);
+            
+            //Assert
+            Assert.True(location.Stock["peppers"].Equals(peppers));
+        }
+    }
+}
