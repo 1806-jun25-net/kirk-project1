3d701d1b76aa0462e23c330b36a3b48e5b4bd9d4
Brandon Kirk <bjkirk94@gmail.com>
2018-06-30 12:44:41 -0400

    Added tests of Location.cs's RemoveStock method

--- PizzaShop/PizzaShop.Testing/LocationsTest.cs
+++ PizzaShop/PizzaShop.Testing/LocationsTest.cs
@@ -7,7 +7,9 @@ using Xunit;
 namespace PizzaShop.Testing
 {
     public class LocationsTest
-    {
+    { 
+
+        //Testing of AddStock()
         [Fact]
         public void AddStockShouldAddNewItemToDictionaryWhenNotAlreadyInSide()
         {
@@ -54,5 +56,114 @@ namespace PizzaShop.Testing
             //Assert
             Assert.True(location.Stock["peppers"].Quantity == 3);
         }
+
+
+        //Testing of RemoveStock()
+
+        [Fact]
+        public void RemoveStockShouldNotCauseExceptionIfAttemptingToRemoveFromEmptyStock()
+        {
+            // Arrange
+            var location = new Location();
+            var peppers = new Topping("peppers", 1);
+
+            //Act
+            location.RemoveStock(peppers);
+
+            //Assert
+            Assert.True(true);//if code reaches here it didn't crash
+        }
+
+        [Fact]
+        public void RemoveStockShouldNotCauseKeyNotFoundExceptionIfAttemptingToRemoveStockOfMissingItem()
+        {
+            // Arrange
+            var location = new Location();
+            var peppers = new Topping("peppers", 1);
+            var onions = new Topping("onions", 7);
+
+            //Act
+            location.RemoveStock(peppers);
+
+            //Assert
+            Assert.True(true);//if code reaches here it didn't crash
+        }
+
+        [Fact]
+        public void RemoveStockShouldReturnIngredientNameWhenItDoesNotExistInStock()
+        {
+            // Arrange
+            var location = new Location();
+            var peppers = new Topping("peppers", 1);
+
+            //Act
+            string name = location.RemoveStock(peppers);
+
+            //Assert
+            Assert.Equal(name, peppers.Name);
+        }
+
+        [Fact]
+        public void RemoveStockShouldReturnIngredientNameWhenSufficientQuantityDoesNotExist()
+        {
+            // Arrange
+            var location = new Location();
+            var peppers1 = new Topping("peppers", 1);
+            var peppers2 = new Topping("peppers", 2);
+
+            //Act
+            string name = location.RemoveStock(peppers2);
+
+            //Assert
+            Assert.Equal(name, peppers1.Name);
+        }
+
+        [Fact]
+        public void RemoveStockShouldNotChangeIngredientQuantityWhenSufficientQuantityDoesNotExist()
+        {
+            // Arrange
+            var location = new Location();
+            var peppers1 = new Topping("peppers", 1);
+            var peppers2 = new Topping("peppers", 2);
+            location.AddStock(peppers1);
+
+            //Act
+            location.RemoveStock(peppers2);
+
+            //Assert
+            Assert.True(location.Stock["peppers"].Quantity == peppers1.Quantity);
+        }
+
+        [Fact]
+        public void RemoveStockShouldDecrementIngredientQuantityWhenSufficientQuantityExists()
+        {
+            // Arrange
+            var location = new Location();
+            var peppers1 = new Topping("peppers", 1);
+            var peppers2 = new Topping("peppers", 2);
+            location.AddStock(peppers2);
+
+            //Act
+            location.AddStock(peppers1);
+
+            //Assert
+            Assert.True(location.Stock["peppers"].Quantity == 1);
+        }
+
+        [Fact]
+        public void RemoveStockShouldRemoveIngredientFromStockDictionaryWhenQuantityReachesZero()
+        {
+            // Arrange
+            var location = new Location();
+            var peppers = new Topping("peppers", 1);
+            location.AddStock(peppers);
+
+            //Act
+            location.RemoveStock(peppers);
+
+            //Assert
+            Assert.True(!(location.Stock.ContainsKey(peppers.Name)));
+        }
+
     }
 }
