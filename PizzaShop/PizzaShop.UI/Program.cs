b6c15ba730c62ab51cbc69fb810c997ad1cc772f
Brandon Kirk <bjkirk94@gmail.com>
2018-06-29 21:40:26 -0400

    added empty testing and ui projects to PizzaShop solution

---
+++ PizzaShop/PizzaShop.UI/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PizzaShop.UI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
