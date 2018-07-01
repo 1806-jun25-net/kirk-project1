89f915cf533b6829378f34d9aa8871be81e6f0f2
Brandon Kirk <bjkirk94@gmail.com>
2018-07-01 19:39:47 -0400

    Corrected iPizza to be interface instead of class.  Added additional functionality to BYOPizza for changing toppings/sauce/crust and tracking price of pizza.

--- PizzaShop/PizzaShop.Library/BuildYourOwnPizza.cs
+++ PizzaShop/PizzaShop.Library/BuildYourOwnPizza.cs
@@ -6,12 +6,63 @@ namespace PizzaShop.Library
 {
     class BuildYourOwnPizza : IPizza
     {
+        public string Size { get; set; }
+        public string CrustType { get; set; }
+        public string SauceType { get; set; }
+        public HashSet<string> Toppings { get; set; }
+        public decimal Price { get; set; }
         public BuildYourOwnPizza (String s)
         {
+            Toppings = new HashSet<String>();
             Size = s;
-            CrustType = new Crust("classic crust", SizingPricingAccessor.SPM.GetIngredientUsageScalar(s));
-            SauceType = new Sauce("classic sauce", SizingPricingAccessor.SPM.GetIngredientUsageScalar(s));
-            Toppings.Add(new Topping("cheese", SizingPricingAccessor.SPM.GetIngredientUsageScalar(s)));
+            CrustType = "classic crust";
+            SauceType = "classic sauce";
+            Toppings.Add("cheese");
+            Price = SizingPricingAccessor.SPM.GetBasePrice(s) + SizingPricingAccessor.SPM.GetToppingPrice(s);
         }
+
+        public void AddTopping(string item)
+        {
+            // Do nothing if topping is already on this pizza, or is not a valid known topping
+            if (Toppings.Contains(item) || !IngredientDirectoryAccessor.ingDir.Toppings.Contains(item))
+                return;
+            Toppings.Add(item);
+            Price += SizingPricingAccessor.SPM.GetToppingPrice(Size);
+        }
+
+        public void RemoveTopping(string item)
+        {
+            // Do nothing if given topping is not currently on pizza
+            if (!Toppings.Contains(item))
+                return;
+            Toppings.Remove(item);
+            Price -= SizingPricingAccessor.SPM.GetToppingPrice(Size);
+        }
+
+        public void ChangeCrust(string item)
+        {
+            //Do nothing if given crust is not a valid crust
+            if (!IngredientDirectoryAccessor.ingDir.Crusts.Contains(item))
+                return;
+            CrustType = item;
+        }
+
+        public void ChangesSauce(string item)
+        {
+            //Do nothing if given sauce is not a valid sauce
+            if (!IngredientDirectoryAccessor.ingDir.Sauces.Contains(item))
+                return;
+            SauceType = item;
+        }
+
+        public void changeSize(string s)
+        {
+            //Do nothing if given size is not a valid size
+            if (!SizingPricingAccessor.SPM.Sizes.Contains(s))
+                return;
+            Size = s;
+            Price = SizingPricingAccessor.SPM.GetBasePrice(s) + SizingPricingAccessor.SPM.GetToppingPrice(s) * Toppings.Count;
+        }
+
     }
 }
--- PizzaShop/PizzaShop.Library/IPizza.cs
+++ PizzaShop/PizzaShop.Library/IPizza.cs
@@ -4,11 +4,12 @@ using System.Text;
 
 namespace PizzaShop.Library
 {
-    public class IPizza
+    public interface IPizza
     {
-        public string Size { get; set; }
-        public Crust CrustType { get; set; }
-        public Sauce SauceType { get; set; }
-        public HashSet<Topping> Toppings { get; }
+        string Size { get; }
+        string CrustType { get; }
+        string SauceType { get; }
+        HashSet<String> Toppings { get; set; }
+        decimal Price { get; }
     }
 }
