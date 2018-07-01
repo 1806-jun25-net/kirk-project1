f7d3f295e913b6f4cbf91d88006f158d47fbfded
Brandon Kirk <bjkirk94@gmail.com>
2018-07-01 10:42:54 -0400

    Added IPizza interface and initial implementation of BuildYourOwnPizza

---
+++ PizzaShop/PizzaShop.Library/BuildYourOwnPizza.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PizzaShop.Library
+{
+    class BuildYourOwnPizza : IPizza
+    {
+        public BuildYourOwnPizza (String s)
+        {
+            Size = s;
+            CrustType = new Crust("classic crust", SizingPricingAccessor.SPM.GetIngredientUsageScalar(s));
+            SauceType = new Sauce("classic sauce", SizingPricingAccessor.SPM.GetIngredientUsageScalar(s));
+            Toppings.Add(new Topping("cheese", SizingPricingAccessor.SPM.GetIngredientUsageScalar(s)));
+        }
+    }
+}
---
+++ PizzaShop/PizzaShop.Library/IPizza.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PizzaShop.Library
+{
+    public class IPizza
+    {
+        public string Size { get; set; }
+        public Crust CrustType { get; set; }
+        public Sauce SauceType { get; set; }
+        public HashSet<Topping> Toppings { get; }
+    }
+}
