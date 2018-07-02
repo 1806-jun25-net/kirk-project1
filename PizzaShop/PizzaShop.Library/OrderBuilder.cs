63d3b0c3b52af85de5fa2435aa15daf1d31b3d65
Brandon Kirk <bjkirk94@gmail.com>
2018-07-01 22:25:58 -0400

    Added OrderBuilder class with several unimplemented members to get it started off.  Intention is for this class to manage and make changes to an order as it is being built, as well as verify an order is valid before placing it.

---
+++ PizzaShop/PizzaShop.Library/OrderBuilder.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PizzaShop.Library
+{
+    class OrderBuilder
+    {
+        public IOrder order;
+        public IPizza ActivePizza { get; set; }
+
+        public OrderBuilder(string user, string store, string size)
+        {
+            order = new Order(user, store);
+            ActivePizza = new BuildYourOwnPizza(size);
+        }
+        public OrderBuilder(Order o)
+        {
+            order = new Order(o.UserID, o.Store, o.Pizzas);
+            ActivePizza = o.Pizzas[0];
+        }
+
+        public void StartNewPizza()
+        {
+
+        }
+
+        public void DuplicatePizza(int i)
+        {
+
+        }
+
+        public void SwitchToPizza(int i)
+        {
+
+        }
+
+        public void AddToppingToCurrentPizza(string topping)
+        {
+            
+        }
+
+        public void ChangeSauceOnCurrentPizza(string sauce)
+        {
+
+        }
+
+        public void ChangeCrustOnCurrentPizza(string crust)
+        {
+            
+        }
+
+        public bool ChangeLocation(string store)
+        {
+            return false;
+        }
+
+        public List<IPizza> GetPizzas()
+        {
+            return null;
+        }
+
+
+
+
+
+
+        public string FinalizeOrder()
+        {
+            return null;
+        }
+
+        public bool IsOrderSmallEnough()
+        {
+            return false;
+        }
+
+        public bool IsOrderCheapEnough()
+        {
+            return false;
+        }
+
+        public bool IsOrderNotEmpty()
+        {
+            return false;
+        }
+
+        public bool IsOrderTwoHoursLater()
+        {
+            return false;
+        }
+    }
+}
