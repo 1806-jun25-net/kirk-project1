bc1e4c68a41bb9364fefd7d40c6bd4d6d77d9cb2
Brandon Kirk <bjkirk94@gmail.com>
2018-07-03 16:34:31 -0400

    Beginning implementing console  menu logic.

--- PizzaShop/PizzaShop.UI/Program.cs
+++ PizzaShop/PizzaShop.UI/Program.cs
@@ -6,7 +6,28 @@ namespace PizzaShop.UI
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string input = "";
+            //Main menu logic all going here for now
+            do
+            {
+                //first startup, not yet logged in
+                Console.WriteLine("Welcome to the pizza ordering system!");
+                Console.WriteLine("Please enter your username, or press ender to create a new user.");
+                input = Console.ReadLine();
+                if (input.Equals(""))
+                {
+                    //CREATE NEW USER
+                }
+                else
+                {
+                    //SEARCH FOR USER IN USER TABLE
+                    // If User found, set user and proceed to menu
+                    //?????Session object??????? for current user & menu state
+
+                    //Else, user not found, prompt to try again
+                }
+            }
+            while (true);
         }
     }
 }
