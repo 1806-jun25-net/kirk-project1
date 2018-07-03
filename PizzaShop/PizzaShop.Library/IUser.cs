d66f17b76c60234b26d465e4471bd6d0849da088
Brandon Kirk <bjkirk94@gmail.com>
2018-07-02 22:58:10 -0400

    Created initial IUser interface and User class with basic properties.

---
+++ PizzaShop/PizzaShop.Library/IUser.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PizzaShop.Library
+{
+    public interface IUser
+    {
+        string FirstName { get; set; }
+        string LastName { get; set; }
+        string Email { get; set; }  
+        string Phone { get; set; }
+        string FavStore { get; set; }
+        string Username { get; set; }
+    }
+}
