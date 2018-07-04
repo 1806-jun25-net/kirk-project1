288132b6dc1edf084578d9bc2d62cffefd8a9c81
Brandon Kirk <bjkirk94@gmail.com>
2018-07-04 02:24:50 -0400

    Slowly improving menu

--- PizzaShop/PizzaShop.UI/Program.cs
+++ PizzaShop/PizzaShop.UI/Program.cs
@@ -1,33 +1,65 @@
-﻿using System;
+﻿using PizzaShop.Library;
+using System;
 
 namespace PizzaShop.UI
 {
     class Program
     {
-        static void Main(string[] args)
+        private static string userID;
+        public static void Main(string[] args)
         {
+            //Run deserialization code
+
+            //Start menu
             string input = "";
-            //Main menu logic all going here for now
+            bool exitApplication = false;
             do
             {
                 //first startup, not yet logged in
-                Console.WriteLine("Welcome to the pizza ordering system!");
-                Console.WriteLine("Please enter your username, or press ender to create a new user.");
+                Console.WriteLine("~~~Welcome to the pizza ordering system!~~~");
+                Console.WriteLine("Please enter the number for your selection");
+                Console.WriteLine("1: New User");
+                Console.WriteLine("2: Existing User");
+                Console.WriteLine("0: Exit Application");
+                Console.Write("->");
                 input = Console.ReadLine();
-                if (input.Equals(""))
-                {
-                    //CREATE NEW USER
-                }
+                if (input.Equals("1"))
+                    //Create New User
+                    MenuNewUser();
+                else if (input.Equals("2"))
+                    //Existing User Login
+                    MenuExistingUserLogin();
+                else if (input.Equals("0"))
+                    //Exit Application
+                    exitApplication = true;
                 else
-                {
-                    //SEARCH FOR USER IN USER TABLE
-                    // If User found, set user and proceed to menu
-                    //?????Session object??????? for current user & menu state
-
-                    //Else, user not found, prompt to try again
-                }
+                    //Invalid Input
+                    Console.WriteLine("Input invalid.  Please try again.");
             }
-            while (true);
+            while (exitApplication == false);
+
+            //Run serialization code one final time
+        }
+
+        public static void MenuNewUser()
+        {
+            Console.WriteLine("~~~New User Creation~~~");
+            Console.WriteLine("Please create a new username:");
+            Console.Write("->");
+        }
+
+        public static void MenuExistingUserLogin()
+        {
+            Console.WriteLine("~~~Existing User Login~~~");
+            Console.WriteLine("Please enter your username:");
+            Console.Write("->");
+            "test".ToUpper();
+            //SEARCH FOR USER IN USER TABLE
+            // If User found, set user and proceed to menu
+            //?????Session object??????? for current user & menu state
+
+            //Else, user not found, prompt to try again
+
         }
     }
 }
